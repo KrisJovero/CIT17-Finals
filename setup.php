dbb71cc1fe886b665762122a5766244449a1bf5c
KrisJovero <natrovez@Gmail.com>
2023-12-13 23:56:00 +0800

    This is my finals

--- setup.php
+++ setup.php
@@ -12,7 +12,7 @@ try {
 
     $conn->exec("USE Jovero");
 
-    $sql = "CREATE TABLE Users (
+    $sql = "CREATE TABLE JoveroK_Users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) NOT NULL,
         password VARCHAR(255) NOT NULL,
@@ -21,37 +21,37 @@ try {
     )";
     $conn->exec($sql);
 
-    $sql = "CREATE TABLE Student (
-        student_id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT,
-        full_name VARCHAR(100) NOT NULL,
-        date_of_birth DATE,
-        address VARCHAR(255),
-        FOREIGN KEY (user_id) REFERENCES Users(id)
-    )";
-    $conn->exec($sql);
-
-    $sql = "CREATE TABLE Course (
-        course_id INT AUTO_INCREMENT PRIMARY KEY,
-        course_name VARCHAR(100) NOT NULL,
-        instructor_id INT,
-        FOREIGN KEY (instructor_id) REFERENCES Instructor(instructor_id)
-    )";
-    $conn->exec($sql);
-
-    $sql = "CREATE TABLE Instructor (
+    $sql = "CREATE TABLE JoveroK_Instructors (
         instructor_id INT AUTO_INCREMENT PRIMARY KEY,
         full_name VARCHAR(100) NOT NULL,
         expertise VARCHAR(100)
     )";
     $conn->exec($sql);
 
-    $sql = "CREATE TABLE Enrollment (
+    $sql = "CREATE TABLE JoveroK_Courses (
+        course_id INT AUTO_INCREMENT PRIMARY KEY,
+        course_name VARCHAR(100) NOT NULL,
+        instructor_id INT,
+        FOREIGN KEY (instructor_id) REFERENCES JoveroK_Instructors(instructor_id)
+    )";
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE JoveroK_Students (
+        student_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        full_name VARCHAR(100) NOT NULL,
+        date_of_birth DATE,
+        address VARCHAR(255),
+        FOREIGN KEY (user_id) REFERENCES JoveroK_Users(id)
+    )";
+    $conn->exec($sql);
+
+    $sql = "CREATE TABLE JoveroK_Enrollment (
         enrollment_id INT AUTO_INCREMENT PRIMARY KEY,
         student_id INT,
         course_id INT,
-        FOREIGN KEY (student_id) REFERENCES Student(student_id),
-        FOREIGN KEY (course_id) REFERENCES Course(course_id)
+        FOREIGN KEY (student_id) REFERENCES JoveroK_Students(student_id),
+        FOREIGN KEY (course_id) REFERENCES JoveroK_Courses(course_id)
     )";
     $conn->exec($sql);
 
